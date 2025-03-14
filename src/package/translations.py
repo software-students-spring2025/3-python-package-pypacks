c8e0a35947dc90211229c88ab36ae6a4b1b9f571
ajokt123 <at4933@nyu.edu>
2025-03-13 21:22:14 -0400

    testers + full_to_slang implementation

--- src/package/translations.py
+++ src/package/translations.py
@@ -1,1 +1,11 @@
-from package import fulltoSlang
+from fullToSlang import fullToSlang
+
+def full_to_slang(words: str) -> str:
+    words = words.lower() 
+    
+    for phrase, slang in fullToSlang.items():
+        words = words.replace(phrase, slang)
+    return words
+
+if __name__ == "__main__":
+    print(full_to_slang("Thank you for you help. I will talk to you later!"))
