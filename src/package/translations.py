17b705c6ccdce60023c41004d93a96d3b5a228dc
ajokt123 <at4933@nyu.edu>
2025-03-16 22:38:53 -0400

    quick adjustment

--- src/package/translations.py
+++ src/package/translations.py
@@ -1,12 +1,13 @@
 #full text to slang words
 from package.fullToSlang import fullToSlang
-def full_to_slang(words: str) -> str:
+def full_to_slang(words):
     words = words.lower()
     for phrase, slang in fullToSlang.items():
         words = words.replace(phrase, slang)
     return words
 
-if __name__ == "__main__":  
+#this is just to check if my function words (Ajok)
+if __name__ == "__main__": 
     sentence = "I don't know. To be honest, this is a test" 
     print(f"Original: {sentence}")
     print(f"Slang:{full_to_slang(sentence)}")
