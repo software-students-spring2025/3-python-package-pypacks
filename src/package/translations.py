314bf92251abf0209a89e9788fd0cf69c52b90d9
nyjur1 <nbilkui@gmail.com>
2025-03-19 19:23:06 -0400

    Add the example function file with function updates

--- src/package/translations.py
+++ src/package/translations.py
@@ -39,31 +39,6 @@ def slang_to_emoji(words):
 
 
 
-
-def full_to_slang(words):
-    words = words.lower()
-    for phrase, slang in fullToSlang.items():
-        words = words.replace(phrase, slang)
-    return words
-
-import re
-
-def slang_to_full(words):
-    words = words.lower()
-    for slang, phrase in SlangToFull.items():
-        pattern = r'\b{}\b'.format(re.escape(slang))
-        words = re.sub(pattern, phrase, words)
-    return words
-
-
-##slang to emojis
-def slang_to_emoji(words):
-    words = str(words).lower()
-    for slang, emoji in slangToEmoji.items():
-        words = words.replace(slang, emoji)
-    return words
-
-## remove slang words from a sentence
 def remove_slang(words):
     words = words.lower()
 
