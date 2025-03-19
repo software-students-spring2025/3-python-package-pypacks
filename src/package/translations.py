92911e7b5441250237c69e7d2edbeaddec57c189
nyjur1 <nbilkui@gmail.com>
2025-03-19 18:52:27 -0400

    example functions with updates on functions

--- src/package/translations.py
+++ src/package/translations.py
@@ -1,12 +1,21 @@
 #full text to slang words
+import string
 from package.fullToSlang import fullToSlang
 from package.slangToEmoji import slangToEmoji
 from package.SlangToFull import SlangToFull
 
+def remove_punctuation(text):
+    translator = str.maketrans('', '', string.punctuation)
+    return text.translate(translator)
+
+
 def full_to_slang(words):
     words = words.lower()
     for phrase, slang in fullToSlang.items():
-        words = words.replace(phrase, slang)
+        pattern = r'\b{}\b'.format(re.escape(phrase))
+        #words = words.replace(phrase, slang)
+        words = re.sub(pattern, slang, words)
+    
     return words
 
 import re
@@ -23,5 +32,13 @@ def slang_to_full(words):
 def slang_to_emoji(words):
     words = str(words).lower()
     for slang, emoji in slangToEmoji.items():
-        words = words.replace(slang, emoji)
+        pattern = r'\b{}\b'.format(re.escape(slang))
+        #words = words.replace(slang, emoji)
+        words = re.sub(pattern, emoji, words)
     return words
+
+
+
+
+
+
