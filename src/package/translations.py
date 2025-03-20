e6ef6d51b562d13ffef3f52a7537eb9007cc1bbd
nyjur1 <nbilkui@gmail.com>
2025-03-19 21:32:42 -0400

    example functions and and function updates

--- src/package/translations.py
+++ src/package/translations.py
@@ -46,3 +46,40 @@ def remove_slang(words):
         words = re.sub(r'\b{}\b'.format(re.escape(slang)), '', words)
 
     return " ".join(words.split())
+
+from translations import full_to_slang, slang_to_full, slang_to_emoji, remove_slang
+
+def main():
+    #example text for all the necessary conversions 
+
+    text = "I love software engineering, omg , I don't know though I really need to stop procrastinating lol! #stem"
+
+    print("Orginal Text:")
+    print(text)
+
+
+    #full text to slang 
+
+    slang_text = full_to_slang(text)
+    print("\nConverted to slang:")
+    print(slang_text)
+
+    #slang to full text 
+
+    full_text = slang_to_full(text)
+    print("\nConverted to full:")
+    print(full_text)
+
+    #slang to emojis 
+    emoji_version =slang_to_emoji(text)
+    print("\nConverted to emoji:")
+    print(emoji_version)
+
+    #removes slang
+    remove_slang_from_text = remove_slang(text)
+    print("\nRemoving all slang:")
+    print(remove_slang_from_text)
+
+if __name__ == "__main__":
+    main()
+
